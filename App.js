5d72ead2c01195aeb08b8a3509d659f799a790ad
shubhvarshney <shubh.varshney@lhswildcats.org>
2025-07-05 16:16:32 -0500

    Edited some config for the backend

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { NativeRouter } from 'react-router-native';
 import { ApolloProvider } from '@apollo/client';
-import { StyleSheet } from 'react-native';
 import Main from './src/components/Main';
 import createApolloClient from './src/utils/apolloClient';
 import AuthStorage from './src/utils/authStorage';
@@ -24,12 +23,3 @@ export default function App() {
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
